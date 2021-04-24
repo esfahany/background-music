91013d451e10f23425227b78b29c98c219b15fd3
Bilqis Ayeni <bilqis@Bilqiss-MacBook-Air.local>
2021-04-24 12:23:32 -0400

    Combining Voice Sentiment and Facial Expression

---
+++ combination/face-script.js
@@ -0,0 +1,60 @@
+const video = document.getElementById('video')
+
+// Use webcam
+startVideo = () => {
+    navigator.getUserMedia(
+      { video: {} },
+      stream => video.srcObject = stream,
+      err => console.error(err)
+    )
+}
+
+// Start the video only after getting the required models
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+]).then(startVideo)
+
+// Function to get the expression with the highest confidence
+function expressionResult(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+// When the webcam is on...
+video.addEventListener('play', () => {
+    setInterval(async () => {
+        // Get facial expression labels from face-api
+        const detections = await faceapi.detectAllFaces(video, 
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+            // Ignore if no face / expression is detected
+            if (!detections.length) {
+                return
+            }
+            // Make expressions the objects of all the expressions and their confidence levels
+            let expressions = detections[0].expressions
+            let expressionsResult =(Object.values(expressions));
+            // Get the highest expression level
+            let highestValue = (Math.max(...expressionsResult));
+            // Find the expression with the highest expression level
+            let result = expressionResult(expressions, highestValue)
+            document.getElementById("expression").innerHTML = result;
+
+
+            // Get the confidence of each expression and add it to the html
+            expressionName = ["neutral", "happy", "sad", "angry", "fearful", "disgusted", "surprised"]
+            let expressionValue=[];
+            for (let index = 0; index < expressionName.length; index++) {
+                const expressionIndex = expressionName[index];
+                expressionValue[index] = expressions[expressionIndex]
+            }
+            for (let index = 0; index < expressionName.length; index++) {
+                const name = expressionName[index];
+                const value = expressionValue[index]
+                let analysis = (name + " is " + value)
+                document.getElementById(name).innerHTML = analysis;
+            }
+    // Checks the expressions every three seconds
+    }, 1000)
+})
